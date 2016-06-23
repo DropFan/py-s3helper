23be5278b99f98647d4927d85450ce527452c611
DropFan <DropFan@Gmail.com>
2016-06-24 00:17:37 +0800

    main method

--- s3helper.py
+++ s3helper.py
@@ -1,3 +1,19 @@
 #! /bin/env python2
 # -*- coding:utf-8 -*-
 # author: Tiger <DropFan@Gmail.com>
+class S3_helper(object):
+    def __init__(self, **kwargs):
+        super(S3_helper, self).__init__()
+    def upload(self, key, filepath):
+        print('upload to s3: key:%s file: %s' % (key, filepath))
+        return False
+    def download(self, key, filename):
+        print('download from s3: key:%s file: %s' % (key, filename))
+        return False
+    def isExists(self, key):
+        print('isExists().  key: %s' % key)
+        return None
+    def delete(self, key):
+        print('delete from s3 (). key: %s' % key)
+        return None
+# end class S3_helper
