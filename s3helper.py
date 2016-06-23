904bd5ebe49a8fd552a60ac010fa5ee7e2a47cf9
DropFan <DropFan@Gmail.com>
2016-06-24 00:19:30 +0800

    init method

--- s3helper.py
+++ s3helper.py
@@ -3,7 +3,39 @@
 # author: Tiger <DropFan@Gmail.com>
 class S3_helper(object):
     def __init__(self, **kwargs):
+        """
+            construct method
+        """
         super(S3_helper, self).__init__()
+
+        self.region = kwargs['region']
+        self.bucketName = kwargs['bucket']
+        self.aws_access_key = kwargs['accessKey']
+        self.aws_secret_key = kwargs['secretKey']
+        if self.bucketName:
+            self.setBucket()
+        # self.url = 'http://%s.s3-website-ap-northeast-1.amazonaws.com/%s'
+        self.url = 'https://s3-%s.amazonaws.com/%s/%s'
+
+    def setBucket(self, bucketName=''):
+        print 'setBucket()  bucketName:%s' % bucketName
+        if not bucketName:
+            bucketName = self.bucketName
+        else:
+            self.bucketName = bucketName
+
+        # connect to the bucket
+        conn = boto.connect_s3(self.aws_access_key, self.aws_secret_key)
+        bucket = conn.get_bucket(bucketName)
+
+        self.conn = conn
+        self.bucket = bucket
+
+        # create a key to keep track of our file in the storage
+        k = Key(bucket)
+        self.k = k
+        print k
+
     def upload(self, key, filepath):
         print('upload to s3: key:%s file: %s' % (key, filepath))
         return False
